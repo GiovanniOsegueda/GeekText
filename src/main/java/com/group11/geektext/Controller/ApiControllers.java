63752961dfef4c137038e26f25e92b7195c4df5b
Giovanni <giovanniosegueda@gmail.com>
2022-06-12 12:23:35 -0400

    Updated main. Now we are able to delete user

--- src/main/java/com/group11/geektext/Controller/ApiControllers.java
+++ src/main/java/com/group11/geektext/Controller/ApiControllers.java
@@ -3,10 +3,7 @@ package com.group11.geektext.Controller;
 import com.group11.geektext.Models.User;
 import com.group11.geektext.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +15,7 @@ public class ApiControllers {
 
     @GetMapping(value = "/")
     public String getPage(){
-        return "Hello From group 11!! WOOT WOOT";
+        return "Hello World From group 11!! WOOT WOOT";
     }
 
     @GetMapping(value = "/users")
@@ -32,5 +29,25 @@ public class ApiControllers {
         return "Congratulation it SAVED!!!!";
     }
 
+    /*
+    @PostMapping(value = "update/{id}")
+    public String updateUser(long id, @RequestBody User user){
+        User updateUser = userRepo.findById(id).get();
+        updateUser.setFirstName(user.getFirstName());
+        updateUser.setLastName(user.getLastName());
+        updateUser.setAge(user.getAge());
+        updateUser.setOccupation(user.getoccupation());
+        userRepo.save(updateUser);
+        return "Updated user!";
+    }
+
+    */
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id){
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Deleted user with id: " + id;
+    }
 
 }
