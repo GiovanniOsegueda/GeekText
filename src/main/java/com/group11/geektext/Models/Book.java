838db4df9aed5ce8fd82604e91dd443d78365729
Giovanni <giovanniosegueda@gmail.com>
2022-06-23 18:51:55 -0400

    added book class , bookRepo and updated API Controllers

---
+++ src/main/java/com/group11/geektext/Models/Book.java
@@ -0,0 +1,122 @@
+package com.group11.geektext.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Column
+    public String bookName;
+
+    @Column
+    public String bookGenre;
+
+    @Column
+    public String bookPublisher;
+
+    @Column
+    public String bookAuthor;
+
+    @Column
+    public String bookDescription;
+
+    @Id
+    @Column
+    public String bookISBN;
+
+    @Column
+    public String bookPrice;
+
+    @Column
+    public String yearPublished;
+
+    @Column
+    public String bookCopiesSold;
+
+    @Column
+    public int bookRating;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookGenre() {
+        return bookGenre;
+    }
+
+    public void setBookGenre(String bookGenre) {
+        this.bookGenre = bookGenre;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookDescription() {
+        return bookDescription;
+    }
+
+    public void setBookDescription(String bookDescription) {
+        this.bookDescription = bookDescription;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setBookISBN(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    public String getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(String bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(String yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String getBookCopiesSold() {
+        return bookCopiesSold;
+    }
+
+    public void setBookCopiesSold(String bookCopiesSold) {
+        this.bookCopiesSold = bookCopiesSold;
+    }
+
+    public int getBookRating() {
+        return bookRating;
+    }
+
+    public void setBookRating(int bookRating) {
+        this.bookRating = bookRating;
+    }
+
+
+}
